078cee330f55c8cb2e9af684e5d1b717e3918662
Mohamad Khan <mohamad@Mohamads-MBP.attlocal.net>
2017-12-13 02:06:58 -0800

    schedule route added

---
+++ src/controller/schedule.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import bodyParser from 'body-parser';
+import config from '../config';
+
+export default ({ config, db }) => {
+  let api = Router();
+
+  api.get('/', (req, res) => {
+    let cart= [];
+    cart = [{
+        "name": "Baker Beach",
+        "Latitude": 37.791032,
+        "Longitude": -122.5096298,
+        "AverageTime": 4.00,
+        "StartTime": 1500,
+        "EndTime": 1700},
+      {
+        "name": "Lombard Street",
+        "Latitude": 37.7918949,
+        "Longitude": -122.4623753,
+        "AverageTime": 1.00,
+        "StartTime": 900,
+        "EndTime": 1700},
+      {
+        "name": "Treasure Island",
+        "Latitude": 37.8201814,
+        "Longitude": -122.3864967,
+        "AverageTime": 1.00,
+        "StartTime": 900,
+        "EndTime": 2300}]
+
+        //Algorithm
+
+        res.send(cart);
+      
+    
+  });
+
+  return api;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import middleware from '../middleware';
 import initalizeDb from '../db';
 import user from '../controller/user';
 import trip from '../controller/trip';
+import schedule from '../controller/schedule';
 
 let router = express();
 
@@ -16,6 +17,7 @@ initalizeDb(db => {
   //api routes v1 (/v1)
   router.use('/user', user({ config, db }));
   router.use('/trip', trip({ config, db }));
+  router.use('/schedule', schedule({ config, db }));
 });
 
 export default router;
