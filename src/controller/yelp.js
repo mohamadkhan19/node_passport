2c3a5442ce88c6bcbbd17190b084666fb3d5a75e
Mohamad Khan <mohamad@Mohamads-MacBook-Pro.local>
2018-02-23 19:25:29 -0800

    yelp route bug fixed

--- src/controller/yelp.js
+++ src/controller/yelp.js
@@ -1,14 +1,14 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import bodyParser from 'body-parser';
-import { authenticate } from '../middleware/authMiddleware'
+import { generateAccessToken, respond, authenticate } from '../middleware/authMiddleware';
 
-const yelp = require('yelp-fusion');
+import yelp from 'yelp-fusion';
 
 const client = yelp.client(process.env.YELP_API);
 
-export default ({config,db}) => {
+export default ({ config, db }) => {
   let api = Router();
-  api.post('/', authenticate,(req,res) => {
+  api.post('/', authenticate ,(req,res) => {
     client.search({
       term: req.body.term,
       location: req.body.location
@@ -16,8 +16,9 @@ export default ({config,db}) => {
       console.log(response.jsonBody.businesses[0].name);
       res.json(response.jsonBody)
     }).catch(e => {
-      console.log(e);
+      res.json(response.jsonBody)
     });
     
   })
+  return api;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ import initalizeDb from '../db';
 import user from '../controller/user';
 import trip from '../controller/trip';
 import schedule from '../controller/schedule';
-import passport from 'passport'
-import yelp from '../controller/yelp'
+import passport from 'passport';
+import yelp from '../controller/yelp';
 
 let router = express();
 
@@ -22,7 +22,7 @@ initalizeDb(db => {
   router.use('/user', user({ config, db }));
   router.use('/trip', trip({ config, db }));
   router.use('/schedule', schedule({ config, db }));
-  router.use('/yelp', yelp({ config, db}));
+  router.use('/yelp', yelp({ config, db }));
   
 });
 
